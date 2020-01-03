86cb05dcaddadbe1b97938ff40804c975b7ddbea
Abhishek Thakur <dev.abhishekthakur@gmail.com>
2020-01-03 18:30:14 +0530

    Adding scipt to check internet speed and log it to a logfile

---
+++ speedtestDaily.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "*************" >>/home/thakur/Documents/speedtestDaily.log
+date >>/home/thakur/Documents/speedtestDaily.log
+speedtest-cli >>/home/thakur/Documents/speedtestDaily.log
+echo "*************" >>/home/thakur/Documents/speedtestDaily.log
+echo "             " >>/home/thakur/Documents/speedtestDaily.log
